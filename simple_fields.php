4230ded8fb94e2c60195953db92b2d819b589912
Pär Thernström <par@earthpeople.se>
2012-08-18 14:51:01 +0200

    - started on a class. classes are nice. - removed that readme again - add link to settings page

--- simple_fields.php
+++ simple_fields.php
@@ -3,7 +3,7 @@
 Plugin Name: Simple Fields
 Plugin URI: http://eskapism.se/code-playground/simple-fields/
 Description: Add groups of textareas, input-fields, dropdowns, radiobuttons, checkboxes and files to your edit post screen.
-Version: 0.6
+Version: 0.x
 Author: Pär Thernström
 Author URI: http://eskapism.se/
 License: GPL2
@@ -26,72 +26,102 @@ License: GPL2
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-// if called directly, load wordpress
-/*
-if (isset($_GET["wp_abspath"])) {
-	define( 'WP_USE_THEMES', false );
-	require( $_GET["wp_abspath"] . './wp-blog-header.php' );
-}
-*/
-
-
-define( "EASY_FIELDS_URL", plugins_url(basename(dirname(__FILE__))). "/");
-define( "EASY_FIELDS_NAME", "Simple Fields"); 
-define( "EASY_FIELDS_VERSION", "0.5");
-#define( "EASY_FIELDS_FILE", "options-general.php?page=simple-fields-options"); // this still feels nasty...
 
 load_plugin_textdomain( 'simple-fields', null, basename(dirname(__FILE__)).'/languages/');
 
-// on admin init: add styles and scripts
-add_action( 'admin_init', 'simple_fields_admin_init' );
-add_action( 'admin_menu', "simple_fields_admin_menu" );
-add_action( 'admin_head', 'simple_fields_admin_head' );
 
-// ajax. that's right baby.
-add_action('wp_ajax_simple_fields_field_group_add_field', 'simple_fields_field_group_add_field');
 
-function simple_fields_admin_init() {
+/**
+ * Class to keep all simple fields stuff together i bit better
+ */ 
+class simple_fields {
 
-	wp_enqueue_script("jquery");
-	wp_enqueue_script("jquery-ui-core");
-	wp_enqueue_script("jquery-ui-sortable");
-	wp_enqueue_script("jquery-ui-dialog");
-	wp_enqueue_style('wp-jquery-ui-dialog');
-	wp_enqueue_script("jquery-effects-highlight");
-	wp_enqueue_script("thickbox");
-	wp_enqueue_style("thickbox");
-	wp_enqueue_script("jscolor", EASY_FIELDS_URL . "jscolor/jscolor.js"); // color picker for type color
-	wp_enqueue_script("date", EASY_FIELDS_URL . "datepicker/date.js"); // date picker for type date
-	wp_enqueue_script("jquery-datepicker", EASY_FIELDS_URL . "datepicker/jquery.datePicker.js"); // date picker for type date
-	wp_enqueue_style('jquery-datepicker', EASY_FIELDS_URL.'datepicker/datePicker.css', false, EASY_FIELDS_VERSION);
 
-	// add css and scripts
-	wp_enqueue_style('simple-fields-styles', EASY_FIELDS_URL.'styles.css', false, EASY_FIELDS_VERSION);
-	wp_register_script('simple-fields-scripts', EASY_FIELDS_URL.'scripts.js', false, EASY_FIELDS_VERSION);
-	wp_localize_script('simple-fields-scripts', 'sfstrings', array(
-		'txtDelete' => __('Delete', 'simple-fields'),
-		'confirmDelete' => __('Delete this field?', 'simple-fields'),
-		'confirmDeleteGroup' => __('Delete this group?', 'simple-fields'),
-		'confirmDeleteConnector' => __('Delete this post connector?', 'simple-fields'),
-		'confirmDeleteRadio' => __('Delete radio button?', 'simple-fields'),
-		'confirmDeleteDropdown' => __('Delete dropdown value?', 'simple-fields'),
-		'adding' => __('Adding...', 'simple-fields'),
-		'add' => __('Add', 'simple-fields'),
-		'confirmRemoveGroupConnector' => __('Remove field group from post connector?', 'simple-fields'),
-		'confirmRemoveGroup' => __('Remove this field group?', 'simple-fields'),
-		'context' => __('Context', 'simple-fields'),
-		'normal' => __('normal'),
-		'advanced' => __('advanced'),
-		'side' => __('side'),
-		'low' => __('low'),
-		'high' => __('high'),
-	));
-	wp_enqueue_script('simple-fields-scripts');
+	/**
+	 * Init is where we setup actions and filers and loads stuff and a little bit of this and that
+	 * 
+	 */
+	function init() {
 
-	define( "EASY_FIELDS_FILE", menu_page_url("simple-fields-options", false) );
+		require("functions_admin.php");
+		require("functions_post.php");
+
+		define( "EASY_FIELDS_URL", plugins_url(basename(dirname(__FILE__))). "/");
+		define( "EASY_FIELDS_NAME", "Simple Fields"); 
+		define( "EASY_FIELDS_VERSION", "0.5");
+
+		// Actions
+		add_action( 'admin_init', array($this, 'admin_init') );
+		add_action( 'admin_menu', "simple_fields_admin_menu" );
+		add_action( 'admin_head', 'simple_fields_admin_head' );
+		add_action('wp_ajax_simple_fields_field_group_add_field', 'simple_fields_field_group_add_field');
+
+		// Filters
+		add_filter( 'plugin_row_meta', array($this, 'set_plugin_row_meta'), 10, 2 );
+
+	}
+
+
+	function admin_init() {
+
+		wp_enqueue_script("jquery");
+		wp_enqueue_script("jquery-ui-core");
+		wp_enqueue_script("jquery-ui-sortable");
+		wp_enqueue_script("jquery-ui-dialog");
+		wp_enqueue_style('wp-jquery-ui-dialog');
+		wp_enqueue_script("jquery-effects-highlight");
+		wp_enqueue_script("thickbox");
+		wp_enqueue_style("thickbox");
+		wp_enqueue_script("jscolor", EASY_FIELDS_URL . "jscolor/jscolor.js"); // color picker for type color
+		wp_enqueue_script("date", EASY_FIELDS_URL . "datepicker/date.js"); // date picker for type date
+		wp_enqueue_script("jquery-datepicker", EASY_FIELDS_URL . "datepicker/jquery.datePicker.js"); // date picker for type date
+		wp_enqueue_style('jquery-datepicker', EASY_FIELDS_URL.'datepicker/datePicker.css', false, EASY_FIELDS_VERSION);
+
+		// add css and scripts
+		wp_enqueue_style('simple-fields-styles', EASY_FIELDS_URL.'styles.css', false, EASY_FIELDS_VERSION);
+		wp_register_script('simple-fields-scripts', EASY_FIELDS_URL.'scripts.js', false, EASY_FIELDS_VERSION);
+		wp_localize_script('simple-fields-scripts', 'sfstrings', array(
+			'txtDelete' => __('Delete', 'simple-fields'),
+			'confirmDelete' => __('Delete this field?', 'simple-fields'),
+			'confirmDeleteGroup' => __('Delete this group?', 'simple-fields'),
+			'confirmDeleteConnector' => __('Delete this post connector?', 'simple-fields'),
+			'confirmDeleteRadio' => __('Delete radio button?', 'simple-fields'),
+			'confirmDeleteDropdown' => __('Delete dropdown value?', 'simple-fields'),
+			'adding' => __('Adding...', 'simple-fields'),
+			'add' => __('Add', 'simple-fields'),
+			'confirmRemoveGroupConnector' => __('Remove field group from post connector?', 'simple-fields'),
+			'confirmRemoveGroup' => __('Remove this field group?', 'simple-fields'),
+			'context' => __('Context', 'simple-fields'),
+			'normal' => __('normal'),
+			'advanced' => __('advanced'),
+			'side' => __('side'),
+			'low' => __('low'),
+			'high' => __('high'),
+		));
+		wp_enqueue_script('simple-fields-scripts');
+
+		define( "EASY_FIELDS_FILE", menu_page_url("simple-fields-options", false) );
+
+	}
+
+	/**
+	 * Add settings link to plugin page
+	 * Hopefully this helps some people to find the settings page quicker
+	 */
+	function set_plugin_row_meta($links, $file) {
+
+		$plugin_plugin_path_and_filename = basename(dirname(__FILE__)) . "/" . basename(__FILE__);
+		if ($file == $plugin_plugin_path_and_filename) {
+			return array_merge(
+				$links,
+				array( sprintf( '<a href="options-general.php?page=%s">%s</a>', "simple-fields-options", __('Settings') ) )
+			);
+		}
+		return $links;
+
+	}
 
 }
 
-require("functions_admin.php");
-require("functions_post.php");
-
+$sf = new simple_fields();
+$sf->init();
