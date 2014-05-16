a0848dd5a96c43f5b1cd60e969446f9fceb6ca51
Pär Thernström <par@earthpeople.se>
2014-05-16 22:51:32 +0200

    travis build started to fail since last commit
    see if going with buddypress-approach fixes it

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,15 +3,40 @@
 #echo "WP_TESTS_DIR is: " . getenv( 'WP_TESTS_DIR' );
 #phpinfo();exit;
 
-require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+// change loading based on this:
+// https://buddypress.trac.wordpress.org/changeset/7421
+
+/*require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+*/
+
+/** 
+ * In the pre-develop.svn WP development environment, an environmental bash 
+ * variable would be set to run PHP Unit tests. However, this has been done 
+ * away with in a post-develop.svn world. We'll still check if this variable 
+ * is set for backwards compat. 
+ */ 
+if ( getenv( 'WP_TESTS_DIR' ) ) { 
+    define( 'WP_TESTS_DIR', getenv( 'WP_TESTS_DIR' ) ); 
+    define( 'WP_TESTS_CONFIG_PATH', WP_TESTS_DIR . '/wp-tests-config.php' ); 
+} 
+else { 
+    define( 'WP_ROOT_DIR', dirname( dirname( dirname( dirname( dirname( __DIR__ ) ) ) ) ) ); 
+    define( 'WP_TESTS_DIR', WP_ROOT_DIR . '/tests/phpunit' ); 
+    define( 'WP_TESTS_CONFIG_PATH', WP_ROOT_DIR . '/wp-tests-config.php' ); 
+} 
+ 
+if ( ! file_exists( WP_TESTS_DIR . '/includes/functions.php' ) ) 
+    die( 'The WordPress PHPUnit test suite could not be found.' ); 
+ 
+require_once WP_TESTS_DIR . '/includes/functions.php'; 
 
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../simple_fields.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
-
+require WP_TESTS_DIR . '/includes/bootstrap.php'; 
 
 class SimpleFields_TestCase extends WP_UnitTestCase {
 	
