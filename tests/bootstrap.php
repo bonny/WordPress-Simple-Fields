d657c4c8075e57f5b89dcc73694d065ee0ab7c42
Pär Thernström <par@earthpeople.se>
2014-05-17 12:56:42 +0200

    debug

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,21 @@
 <?php
 
-echo __FILE__;
+echo "\n" . __FILE__ . " " . __LINE__ . "\n";
+echo "\ngetcwd: " . getcwd();
 
 require( dirname( __FILE__ ) . '/includes/define-constants.php' );
-
+echo "\n" . __FILE__ . " " . __LINE__ . "\n";
 if ( ! file_exists( WP_TESTS_DIR . '/includes/functions.php' ) ) {
 	die( "The WordPress PHPUnit test suite could not be found.\n" );
 }
-
+echo "\n" . __FILE__ . " " . __LINE__ . "\n";
 require_once WP_TESTS_DIR . '/includes/functions.php';
-
+echo "\n" . __FILE__ . " " . __LINE__ . "\n";
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../simple_fields.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
+echo "\n" . __FILE__ . " " . __LINE__ . "\n";
 require WP_TESTS_DIR . '/includes/bootstrap.php';
 
 
